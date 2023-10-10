1e99b03f4c1a07136146a766d75f2676f84c970b
ssenerg <amirhossein.pdn@gmail.com>
2023-10-10 13:40:53 +0000

    global spam added

--- telegram_bot/plugins/__init__.py
+++ telegram_bot/plugins/__init__.py
@@ -1,5 +1,6 @@
 from telegram_bot.plugins.scheduled_spam import scheduled_spam
 from telegram_bot.plugins.auto_reaction import auto_reaction
+from telegram_bot.plugins.global_spam import global_spam
 from telegram_bot.plugins.tiktok import tiktok
 from telegram_bot.plugins.reels import reels
 from telegram_bot.plugins.spam import spam
@@ -9,6 +10,7 @@ from typing import List, Callable
 PLUGINS: List[Callable] = [
     scheduled_spam,
     auto_reaction,
+    global_spam,
     reels, 
     tiktok,
     spam,
---
+++ telegram_bot/plugins/global_spam.py
@@ -0,0 +1,44 @@
+from telethon.events import NewMessage
+from telegram_bot import yaml_parser
+from telethon import TelegramClient
+import asyncio
+
+
+async def global_spam(bot: TelegramClient) -> None:
+
+    cfg = yaml_parser()
+
+    @bot.on(NewMessage(outgoing=True))
+    async def listener(event) -> None:
+
+        receiver_id = event.message.to_id.user_id
+        username = await bot.get_entity(receiver_id)
+        username = username.username
+
+        for plan in cfg:
+            if event.message.text.startswith(plan['trigger_message']):
+                for i in range(plan['response_count']):
+                    if plan['sleep'] != 0:
+                        await asyncio.sleep(plan['sleep'])
+                    await bot.send_message(
+                        receiver_id, 
+                        plan['response_message'].replace(
+                            plan['mutiple_character'],
+                            plan['mutiple_character'] * (i + 1)
+                        )
+                    )
+                if not plan['reverse']:
+                    continue
+                for i in range(plan['response_count'] - 1, -1, -1):
+                    if plan['sleep'] != 0:
+                        await asyncio.sleep(plan['sleep'])
+                    await bot.send_message(
+                        receiver_id, 
+                        plan['response_message'].replace(
+                            plan['mutiple_character'],
+                            plan['mutiple_character'] * (i + 1)
+                        )
+                    )
+
+
+__all__ = ['global_spam']
